7944b90b4f356bc5bac7fef5ede82fc5deb646af
OmgCopito95 <july1995bolt@gmail.com>
2020-09-22 18:47:57 -0300

    Initial commit

---
+++ tdp1/Objects/system_stm32f10x.d
@@ -0,0 +1,11 @@
+./objects/system_stm32f10x.o: RTE\Device\STM32F103C8\system_stm32f10x.c \
+  C:\Users\Mauri\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\Mauri\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\Mauri\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Include\cmsis_version.h \
+  C:\Users\Mauri\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Include\cmsis_compiler.h \
+  C:\Users\Mauri\AppData\Local\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  C:\Users\Mauri\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
